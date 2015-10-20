2a5254c11e540a910d12434f22f9e1f9dacd165b
Leon Derczynski <leonderczynski@gmail.com>
2015-10-20 17:32:39 +0200

    moderate updates to regex code

--- ner-scripts/thinner.py
+++ ner-scripts/thinner.py
@@ -5,8 +5,9 @@ from pyspark import SparkContext, SparkConf
 import re
 import twokenize_wrapper
 
+sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout) 
 subreddits = ['4chan', 'worldnews', 'politics', 'soccer', 'technology', 'science', 'android', 'askscience', 'europe', 'sweden', 'gaming', 'movies', 'music', 'news', 'books', 'television', 'technology', 'sports', 'space', 'history', 'philosophy', 'art', 'programming', 'labouruk', 'harrypotter', 'conservative', 'unitedkingdom', 'formula1', 'lego', 'apple', 'seattle', 'libertarian', 'doctorwho', 'scifi', 'economics', 'travel', 'drugs', 'business', 'canada', 'environment', 'astronomy', 'batman', 'gardening', 'health', 'unitedkingdom', 'starwars', 'britishproblems', 'australia', 'engineering', 'casualconversation', 'startrek', 'literature', 'horror', 'google', 'chicago', 'linguistics', 'finance', 'coding', 'toronto', 'ireland', 'tea', 'thenetherlands', 'windows', 'energy', 'introvert', 'privacy', 'feminism', 'socialism', 'newzealand', 'france', 'politicalhumor', 'sanfrancisco', 'education', 'nhl', 'london', 'india', 'law', 'vegetarian', 'mexico', 'ukpolitics', 'worldevents', 'vancouver', 'bayarea', 'brasil', 'wikileaks', 'houston', 'boxing', 'atlanta', 'texas', 'liverpoolfc', 'anonymous', 'denver', 'rugbyunion', 'inthenews', 'germany', 'women', 'football', 'cricket', 'china', 'cycling', 'askacademia', 'chelseafc', 'israel']
-subreddit_re = re.compile(r'subreddit":"(' + '|'.join(subreddits) + ')"', re.I)
+subreddit_re = re.compile(r'subreddit":"(' + '|'.join(subreddits) + ')', re.I)
 
 def thinposts(lines):
 
@@ -17,9 +18,13 @@ def thinposts(lines):
             continue
 
         comment = json.loads(line)
-        comment['tokens'] = twokenize_wrapper.tokenize(comment['body'])
 
-        yield json.dumps(comment)
+        if comment['body'] == '[deleted]':
+            continue
+    
+        if comment['subreddit'].lower() in subreddits:
+            reformed_text = ' '.join(twokenize_wrapper.tokenize(comment['body']))
+            yield reformed_text.strip() + ' <EOS>'
 
 if __name__ == "__main__":
 
@@ -57,8 +62,7 @@ if __name__ == "__main__":
 
     # clean the posts and write them into HDFS from their respective paritions
     print("Writing to HDFS")
-    outjson = rawPostsFile.mapPartitions(thinposts, preservesPartitioning=True)\
-        .saveAsTextFile(outlocation)
+    outjson = rawPostsFile.mapPartitions(thinposts, preservesPartitioning=True).saveAsTextFile(outlocation)
 
 
     sc.stop()
