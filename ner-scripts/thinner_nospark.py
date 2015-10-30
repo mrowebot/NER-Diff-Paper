0cb3f2f79a0463f0eaab1ab2c02625b96c5727c3
L. Derczynski <ac1ld@testnode02.iceberg.shef.ac.uk>
2015-10-30 11:16:39 +0000

    get thinner to run in py3 on sge

--- ner-scripts/thinner_nospark.py
+++ ner-scripts/thinner_nospark.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-import codecs
 import json
-import locale
 import re
 import sys
 
-sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout) 
 subreddits = ['4chan', 'worldnews', 'politics', 'soccer', 'technology', 'science', 'android', 'askscience', 'europe', 'sweden', 'gaming', 'movies', 'music', 'news', 'books', 'television', 'technology', 'sports', 'space', 'history', 'philosophy', 'art', 'programming', 'labouruk', 'harrypotter', 'conservative', 'unitedkingdom', 'formula1', 'lego', 'apple', 'seattle', 'libertarian', 'doctorwho', 'scifi', 'economics', 'travel', 'drugs', 'business', 'canada', 'environment', 'astronomy', 'batman', 'gardening', 'health', 'unitedkingdom', 'starwars', 'britishproblems', 'australia', 'engineering', 'casualconversation', 'startrek', 'literature', 'horror', 'google', 'chicago', 'linguistics', 'finance', 'coding', 'toronto', 'ireland', 'tea', 'thenetherlands', 'windows', 'energy', 'introvert', 'privacy', 'feminism', 'socialism', 'newzealand', 'france', 'politicalhumor', 'sanfrancisco', 'education', 'nhl', 'london', 'india', 'law', 'vegetarian', 'mexico', 'ukpolitics', 'worldevents', 'vancouver', 'bayarea', 'brasil', 'wikileaks', 'houston', 'boxing', 'atlanta', 'texas', 'liverpoolfc', 'anonymous', 'denver', 'rugbyunion', 'inthenews', 'germany', 'women', 'football', 'cricket', 'china', 'cycling', 'askacademia', 'chelseafc', 'israel']
 subreddit_re = re.compile(r'subreddit":"(' + '|'.join(subreddits) + ')', re.I)
 
