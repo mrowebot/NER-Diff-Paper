ebe2942b1f020c7386628fceecac8defa5c26e51
mattroweshow <m.rowe@lancaster.ac.uk>
2015-11-05 09:39:38 +0000

    updated plots

--- diffusion-scripts/cascade_shape_mining.py
+++ diffusion-scripts/cascade_shape_mining.py
@@ -19,6 +19,42 @@ def computeGlobalCascadeIsomorphicDistribution():
         replies = replies1 + replies2
         return replies
 
+    def deriveEntityToPosts(json_line):
+        entity_posts = []
+        json_obj = json.loads(json_line)
+        post_id = json_obj['name']
+        entities = json_obj['entity_texts']
+        for entity in entities:
+            try:
+                entity_posts.append((str(entity), str(post_id)))
+            except:
+                pass
+        return entity_posts
+
+
+    ###### Graph Isomorphism Functions
+    # input: list of posts that the entity has appeared in + list of chains in which the entity has been cited
+    # output: list of canonical entity chains
+    def induce_type1_cascades(entity_citation_posts, chains):
+        for chain in chains:
+            # new chain formed by filtering the chain with only entity citing posts
+            new_chain = set()
+            for chain_edge in chain:
+                source = chain_edge.split("->")[0]
+                target = chain_edge.split("->")[1]
+                if source in entity_citation_posts and target in entity_citation_posts:
+                    new_chain.add(chain_edge)
+            # Ensure that the chain is connected after filtering
+            filtered_chain = set()
+            filterd_source_set = [chain_edge.split("->")[0] for chain_edge in new_chain]
+            filterd_target_set = [chain_edge.split("->")[1] for chain_edge in new_chain]
+            for chain_edge in new_chain:
+                source = chain_edge.split("->")[0]
+                target = chain_edge.split("->")[1]
+
+
+
+
 
     ###### Execution code
     conf = SparkConf().setAppName("NER Diffusion - Cascade Pattern Mining")
@@ -28,7 +64,7 @@ def computeGlobalCascadeIsomorphicDistribution():
     conf.set("spark.default.parallelism", "12")
     conf.set("spark.mesos.coarse", "true")
     conf.set("spark.driver.maxResultSize", "10g")
-    conf.set("spark.cores.max", "5")
+    conf.set("spark.cores.max", "10")
     conf.setMaster("mesos://zk://scc-culture-slave9.lancs.ac.uk:2181/mesos")
     conf.set("spark.executor.uri", "hdfs://scc-culture-mind.lancs.ac.uk/lib/spark-1.3.0-bin-hadoop2.4.tgz")
     conf.set("spark.broadcast.factory", "org.apache.spark.broadcast.TorrentBroadcastFactory")
@@ -39,32 +75,100 @@ def computeGlobalCascadeIsomorphicDistribution():
     # use sample directory for testing
     annotationFile = sc.textFile("hdfs://scc-culture-mind.lancs.ac.uk/reddit/annotated")
     annotationFile.cache()
-    thinnedFile = sc.textFile("hdfs://scc-culture-mind.lancs.ac.uk/reddit/thinned-json")
+    # thinnedFile = sc.textFile("hdfs://scc-culture-mind.lancs.ac.uk/reddit/thinned-json")
+    thinnedFile = sc.textFile("hdfs://scc-culture-mind.lancs.ac.uk/reddit/thinned-json-sample")
     thinnedFile.cache()
 
     # Load the reply graphs from the thinnedFile
     reply_map_rdd = thinnedFile\
-        .flatMap(deriveReplyMap)\
-        .reduceByKey()
+        .flatMap(deriveReplyMap)
     reply_map_rdd.cache()
 
     # get the: {reply, orig} dictionary
     reply_orig_map = reply_map_rdd\
         .collectAsMap()
     print("Reply Orig Map Size = " + str(len(reply_orig_map)))
-    # get the: {orig, [reply]} dictionary
+    # # get the: {orig, [reply]} dictionary
     orig_replies_map = reply_map_rdd\
         .map(lambda x: (x[1], x[0]))\
         .reduceByKey(combineReplies)\
         .collectAsMap()
     print("Orig Replies Map Size = " + str(len(orig_replies_map)))
+
+    # Load the entity to post map
+    # input: json_line of annotations of each post
+    # output: [(entity, [post])] of posts where entities appeared
+    entity_posts_rdd = annotationFile\
+        .flatMap(deriveEntityToPosts)\
+        .reduceByKey(lambda p1, p2: p1 + p2)
+    entity_posts_rdd_map = entity_posts_rdd.collect()
+    print("Entity to posts Map Size = " + str(len(entity_posts_rdd_map)))
+
     #
     #
+    # orig_replies_map = {}
+    # # entities = []
+    # # posts = []
     #
-    # entities = []
-    # posts = []
-    # cascades = {}
-    # for entity in entities:
+    # # Get the cascade graphs for each entity
+    # for entity in entity_posts_rdd_map:
+    #     entity_chains = []
+    #     chain_posts = set()
+    #
+    #     # get the chain that each post was in
+    #     for post in entity_posts_rdd_map[entity]:
+    #         # Do this to speed up computation by ensuring that we haven't already recorded the post in another chain
+    #         if post not in chain_posts:
+    #             chain_posts.add(post)
+    #             chain = set()
+    #             toProcess = []
+    #
+    #             # Starting from the seed post in a possible chain
+    #             # get the replies to the post - down the chain
+    #             if post in orig_replies_map:
+    #                 replies = orig_replies_map[post]
+    #                 toProcess += replies
+    #                 chain.add([reply + "->" + post for reply in replies])
+    #             # Get the post that the post replied to
+    #             if post in reply_orig_map:
+    #                 orig_post = reply_orig_map[post]
+    #                 toProcess += orig_post
+    #                 chain.add(post + "->" + orig_post)
+    #
+    #             # Go through each post that is to be processed
+    #             while len(toProcess) > 0:
+    #                 to_process_post = toProcess.pop()
+    #                 # log that the post has been processed in an already found chain
+    #                 chain_posts.add(to_process_post)
+    #                 # get the replies to this post
+    #                 if to_process_post in orig_replies_map:
+    #                     replies = orig_replies_map[to_process_post]
+    #                     toProcess += replies
+    #                     chain.add([reply + "->" + to_process_post for reply in replies])
+    #                 # get the post that this post relied to
+    #                 if to_process_post in reply_orig_map:
+    #                     orig_post = reply_orig_map[to_process_post]
+    #                     toProcess += orig_post
+    #                     chain.add(to_process_post + "->" + orig_post)
+    #             # log the chain for the entity
+    #             if entity in entity_chains:
+    #                 entity_chains[entity] += sorted(chain)
+    #             else:
+    #                 entity_chains[entity] = sorted(chain)
+
+    # Record the entity patterns depending on the type of pattern to infer and log the canonical form
+    # Type 1: strict consecutive patterns
+    # Type 2: 2-hop scan bounds (i.e. transitive link)
+    # Type 3: keep bounds, highlight citations
+
+
+
+
+
+
+
+
+
     #     # get the posts that the entity was cited in:
     #     for post in posts:
     #         # was the post in a chain?
--- diffusion-scripts/cluster_exploratory_plots.py
+++ diffusion-scripts/cluster_exploratory_plots.py
@@ -1,1 +1,106 @@
+__author__ = 'rowem'
 
+import json
+from datetime import datetime, timedelta
+from pyspark import SparkContext, SparkConf
+import re
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def computeEntityTSData():
+
+    ##### Spark Functions
+    # input: json_line
+    # [(entity: {date, count})]
+    def deriveEntityToDate(json_line):
+        entity_dates = []
+
+        json_obj = json.loads(json_line)
+        created_string = float(json_obj['created_utc'])
+        created_date = datetime.fromtimestamp(created_string).date()
+        annotations = json_obj['entity_texts']
+        for annotation in annotations:
+            date_count = {}
+            date_count[created_date] = 1
+            entity_dates.append((annotation, date_count))
+        return entity_dates
+
+    def combineDateCounts(date_counts1, date_counts2):
+        date_counts = date_counts1
+        for date in date_counts2:
+            if date in date_counts:
+                date_counts[date] += date_counts2[date]
+            else:
+                date_counts[date] = date_counts2[date]
+        return date_counts
+
+
+    ###### Execution code
+    conf = SparkConf().setAppName("NER Diffusion - Exploratory Plots")
+    conf.set("spark.python.worker.memory","10g")
+    conf.set("spark.driver.memory","15g")
+    conf.set("spark.executor.memory","10g")
+    conf.set("spark.default.parallelism", "12")
+    conf.set("spark.mesos.coarse", "true")
+    conf.set("spark.driver.maxResultSize", "10g")
+    # Added the core limit to avoid resource allocation overruns
+    conf.set("spark.cores.max", "5")
+    conf.setMaster("mesos://zk://scc-culture-slave9.lancs.ac.uk:2181/mesos")
+    conf.set("spark.executor.uri", "hdfs://scc-culture-mind.lancs.ac.uk/lib/spark-1.3.0-bin-hadoop2.4.tgz")
+    conf.set("spark.broadcast.factory", "org.apache.spark.broadcast.TorrentBroadcastFactory")
+
+    sc = SparkContext(conf=conf)
+    sc.setCheckpointDir("hdfs://scc-culture-mind.lancs.ac.uk/data/checkpointing")
+
+    # use sample directory for testing
+    # distFile = sc.textFile("hdfs://scc-culture-mind.lancs.ac.uk/user/derczynskil/RC_2015-01")
+    distFile = sc.textFile("hdfs://scc-culture-mind.lancs.ac.uk/reddit/annotated")
+    # Point to local file until data has finished uploading to HDFS
+    # distFile = sc.textFile("/home/derczynskil/annotated/")
+    distFile.cache()
+
+    # Step 1: Derive the time-sensitive map of when entities appeared
+    print("----Loading entity time-series")
+    entity_citation_dates = distFile\
+        .flatMap(deriveEntityToDate)\
+        .reduceByKey(combineDateCounts)
+    entity_citation_dates.cache()
+    # print(entity_citation_dates.collect())
+
+    print("----Deriving the count of entity citations")
+    entity_citation_counts = entity_citation_dates\
+        .map(lambda x: (x[0], len(x[1])))\
+        .map(lambda x: (x[1], x[0]))\
+        .sortByKey(False)\
+        .map(lambda x: (x[1], x[0]))\
+        .collect()
+
+    # Write to local disk
+    print("------Writing the output to a file")
+    outputString = ""
+    for (entity, count) in entity_citation_counts:
+        outputString += str(entity.encode('utf-8')).replace("'", "") + "\t" + str(count) + "\n"
+    # print(outputString)
+    outputFile = open("data/entity_mention_frequencies.csv", "w")
+    outputFile.write(outputString)
+    outputFile.close()
+
+    # Write the time-series output to local disk
+    print("------Writing the ts output to a file")
+    outputString = ""
+    for (entity, date_to_count) in entity_citation_dates.collect():
+        outputString += str(entity.encode('utf-8')).replace("'", "")
+        for date in date_to_count:
+            outputString += "\t" + str(date) + "|" + str(date_to_count[date])
+        outputString += "\n"
+    # print(outputString)
+    outputFile = open("data/entity_mention_ts.csv", "w")
+    outputFile.write(outputString)
+    outputFile.close()
+
+    # stop the Spark context from running
+    sc.stop()
+
+#### Main code execution area
+if __name__ == "__main__":
+    computeEntityTSData()
